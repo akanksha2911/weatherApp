e2aac5831df316159ad27f12f12196ac7f51a97b
akanksha2911 <akanksha1913054@akgec.ac.in>
2023-07-13 23:56:25 +0530

    added choice

--- mysite/weather/views.py
+++ mysite/weather/views.py
@@ -4,27 +4,36 @@ import requests
 import urllib.request
 import json
 
-
-
-# Create your views here.
 def index(request):
     try:
         if request.method == "POST":
            latitude = request.POST['latitude']
            longitude = request.POST['longitude']
-           response=requests.get('https://api.openweathermap.org/data/2.5/weather?lat='+ latitude + '&lon=' + longitude + '&appid=dc3c506bb053361d700130f009aef134&units=metric').json()
-           data = {
+           choice = request.POST['choice']
+           if choice == 'Current weather Minute forecast for 1 hour':
+            response=requests.get('https://api.openweathermap.org/data/2.5/weather?lat='+ latitude + '&lon=' + longitude + '&appid=dc3c506bb053361d700130f009aef134&units=metric').json()
+            data = {
               'info' : response,
               'weather_data': response['weather'][0]
             }
+            
+           elif choice == 'Hourly forecast for 48 hours':
+            response=requests.get('https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=' + latitude + '&lon=' + longitude + '&appid=dc3c506bb053361d700130f009aef134&units=metric').json()
+            data = {
+                'info' : response
+            }
+            
+           else:
+            response=requests.get('api.openweathermap.org/data/2.5/forecast/daily?lat=' + latitude + '&lon=' + longitude + '&cnt={7}&appid=dc3c506bb053361d700130f009aef134&units=metric').json()
+            data = {
+                'info':response
+            }
+        
         else:
            data = {}
         return render(request,'index.html',data)
 
     except:
         return render(request, '404.html')
-    # response=requests.get('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=dc3c506bb053361d700130f009aef134')
-    # users = response.json()
-    # print(users)
-    # return render(request,'index.html')
+    
     
